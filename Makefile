e1c7cde1a0108617bcaadc637f6241c0909c6599
jacob <jacob@jacob.remcomp.fr>
2011-04-11 17:03:07 +0000

    First version of the valarray container

--- Makefile
+++ Makefile
@@ -3,19 +3,25 @@
 # Creates the library libccl.a and a small test program "dotest"
 # 
 #---------------------------------------------------
-CFLAGS=-g -Wno-pointer-sign -DUNIX -Wall -pedantic 
+# Optimized CFLAGS setting
+CFLAGS=-O2 -Wno-pointer-sign -DUNIX -Wall
+# Debug CFLAGS setting
+#CFLAGS=-g -Wno-pointer-sign -DUNIX -Wall 
 SRC=	vector.c bloom.c containererror.c dlist.c qsortex.c heap.c \
 	deque.c hashtable.c malloc_debug.c containers.h \
 	stdint.h pool.c pooldebug.c redblacktree.c scapegoat.c smallpool.c ccl_internal.h \
 	bitstrings.c dictionary.c list.c strcollection.c searchtree.c \
-	containers.h redblacktree.c fgetline.c generic.c queue.c buffer.c observer.c
+	containers.h redblacktree.c fgetline.c generic.c queue.c buffer.c observer.c \
+	valarraydouble.c valarraysize_t.c valarrayint.c valarraylongdouble.c valarraygen.c \
+	valarrayshort.c valarrayfloat.c
 DOCS=
 MAKEFILES=Makefile Makefile.lcc Makefile.msvc
 
 OBJS=vector.o containererror.o dlist.o qsortex.o bitstrings.o generic.o \
     dictionary.o list.o strcollection.o searchtree.o heap.o malloc_debug.o \
     bloom.o fgetline.o pool.o pooldebug.o redblacktree.o scapegoat.o queue.o \
-    buffer.o observer.o
+    buffer.o observer.o valarraydouble.o valarrayint.o valarraysize_t.o \
+    valarraylongdouble.o valarrayshort.o valarrayfloat.o
 
 dotest:	libccl.a test.o
 	gcc -o dotest $(CFLAGS) test.c libccl.a -lm
@@ -25,4 +31,13 @@ clean:
 	rm -rf $(OBJS) libccl.a dotest dotest.dSYM
 zip:	$(SRC)
 	rm container-lib-src.zip;rm -rf ccl;svn export . ccl;zip -9 -r  container-lib-src.zip ccl 
+
+valarraylongdouble.o:   valarraygen.c valarraylongdouble.c containers.h valarraygen.h
+valarraydouble.o:       valarraygen.c valarraydouble.c containers.h valarraygen.h
+valarrayint.o:          valarraygen.c valarrayint.c containers.h valarraygen.h
+valarrayshort.o:	valarraygen.c valarrayshort.c containers.h valarraygen.h
+valarraysize_t.o:       valarraygen.c valarraysize_t.c containers.h valarraygen.h
+valarrayfloat.o:	valarraygen.c valarrayfloat.c containers.h valarraygen.h
+observer.o:	containers.h observer.c valarraygen.h
+buffer.o:	containers.h buffer.c
 
---
+++ valarray.h
@@ -0,0 +1,145 @@
+/****************************************************************************
+ *          ValArraySize_t                                                *
+ ****************************************************************************/
+#undef ElementType
+#undef ValArrayInterface
+#undef ElementType
+#undef ValArray
+#undef tagValArray
+#undef _ValArray
+#define _ValArray _ValArraySize_t
+#define tagValArray tagValArraySize_tInterface
+#define ElementType size_t
+#define ValArrayInterface ValArraySize_tInterface
+#define ValArray ValArraySize_t
+#include "valarraygen.h"
+extern ValArraySize_tInterface iValArraySize_t;
+
+/****************************************************************************
+ *          ValArrayShort                                                   *
+ ****************************************************************************/
+#undef ElementType
+#undef ValArrayInterface
+#undef ElementType
+#undef ValArray
+#undef tagValArray
+#undef _ValArray
+#define _ValArray _ValArrayShort
+#define tagValArray tagValArrayShortInterface
+#define ElementType short
+#define ValArrayInterface ValArrayShortInterface
+#define ValArray ValArrayShort
+#include "valarraygen.h"
+extern ValArrayInterface iValArrayShort;
+
+/****************************************************************************
+ *          ValArrayInt                                                     *
+ ****************************************************************************/
+#undef ElementType
+#undef ValArrayInterface
+#undef ElementType
+#undef ValArray
+#undef tagValArray
+#undef _ValArray
+#define _ValArray _ValArrayInt
+#define tagValArray tagValArrayIntInterface
+#define ElementType int
+#define ValArrayInterface ValArrayIntInterface
+#define ValArray ValArrayInt
+#include "valarraygen.h"
+extern ValArrayInterface iValArrayInt;
+
+/****************************************************************************
+ *          ValArrayLong                                                    *
+ ****************************************************************************/
+#undef ElementType
+#undef ValArrayInterface
+#undef ElementType
+#undef ValArray
+#undef tagValArray
+#undef _ValArray
+#define _ValArray _ValArrayLong
+#define tagValArray tagValArrayLongInterface
+#define ElementType long
+#define ValArrayInterface ValArrayLongInterface
+#define ValArray ValArrayLong
+#include "valarraygen.h"
+extern ValArrayInterface iValArrayLong;
+
+
+/****************************************************************************
+ *          ValArrayDouble                                                  *
+ ****************************************************************************/
+#undef ElementType
+#undef ValArrayInterface
+#undef ElementType
+#undef ValArray
+#undef tagValArray
+#undef _ValArray
+#define _ValArray _ValArrayDouble
+#define tagValArray tagValArrayDoubleInterface
+#define ElementType double
+#define ValArrayInterface ValArrayDoubleInterface
+#define ValArray ValArrayDouble
+#include "valarraygen.h"
+extern ValArrayDoubleInterface iValArrayDouble;
+
+/****************************************************************************
+ *          ValArrayLongDouble                                              *
+ ****************************************************************************/
+#undef ElementType
+#undef ValArrayInterface
+#undef ElementType
+#undef ValArray
+#undef tagValArray
+#undef _ValArray
+#define _ValArray _ValArrayLongDouble
+#define tagValArray tagValArrayLongDoubleInterface
+#define ElementType long double
+#define ValArrayInterface ValArrayLongDoubleInterface
+#define ValArray ValArrayLongDouble
+#include "valarraygen.h"
+extern ValArrayInterface iValArrayLongDouble;
+
+/****************************************************************************
+ *          ValArrayLongLong                                                *
+ ****************************************************************************/
+#undef ElementType
+#undef ValArrayInterface
+#undef ElementType
+#undef ValArray
+#undef tagValArray
+#undef _ValArray
+#define _ValArray _ValArrayLongLong
+#define tagValArray tagValArrayLongLongInterface
+#define ElementType long long
+#define ValArrayInterface ValArrayLongLongInterface
+#define ValArray ValArrayLongLong
+#include "valarraygen.h"
+extern ValArrayInterface iValArrayLongLong;
+
+/****************************************************************************
+ *          ValArrayFloat                                                *
+ ****************************************************************************/
+#undef ElementType
+#undef ValArrayInterface
+#undef ElementType
+#undef ValArray
+#undef tagValArray
+#undef _ValArray
+#define _ValArray _ValArrayFloat
+#define tagValArray tagValArrayFloatInterface
+#define ElementType float
+#define ValArrayInterface ValArrayFloatInterface
+#define ValArray ValArrayFloat
+#include "valarraygen.h"
+extern ValArrayInterface iValArrayFloat;
+
+
+#undef ElementType
+#undef ValArrayInterface
+#undef ElementType
+#undef ValArray
+#undef tagValArray
+#undef _ValArray
+
