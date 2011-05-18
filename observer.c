eeb511cca3c4f975e56a6a59c8e5cf90effdf79b
jacob <jacob@jacob.remcomp.fr>
2011-05-18 17:06:36 +0000

    Streamlined observer.c

--- observer.c
+++ observer.c
@@ -10,72 +10,51 @@ static Observer *ObserverVector;
 static size_t vsize;
 #define CHUNK_SIZE	25
 
-static int initVector(void)
-{
-	ObserverVector = calloc(sizeof(Observer),CHUNK_SIZE);
-	if (ObserverVector == NULL) {
-		iError.RaiseError("iObserver.Subscribe",CONTAINER_ERROR_NOMEMORY);
-		return 0;
-	}
-	vsize=CHUNK_SIZE;
-	return 1;
-}
-
-static int  AddObject(Observer *ob)
-{
-	size_t i;
-	Observer *tmp;
-	
-
-	for (i=0; i<vsize;i++) {
-		if (ObserverVector[i].ObservedObject==NULL) {
-			memcpy(ObserverVector+i,ob,sizeof(Observer));
-			return 1;
-		}
-	}
-	tmp = realloc(ObserverVector,(vsize+CHUNK_SIZE)*sizeof(Observer));
-	if (tmp == NULL) {
-		iError.RaiseError("iObserver.Subscribe",CONTAINER_ERROR_NOMEMORY);
-		return CONTAINER_ERROR_NOMEMORY;
-	}
-	ObserverVector = tmp;
-	memset(ObserverVector+vsize+1,0,(CHUNK_SIZE-1)*sizeof(Observer));
-	memcpy(ObserverVector+vsize,ob,sizeof(Observer));
-	vsize+= CHUNK_SIZE;
-	return 1;
-}
-
-static int InitObserver(Observer *result,void *ObservedObject, ObserverFunction callback, unsigned flags)
-{
-	GenericContainer *gen = ObservedObject;
-	unsigned Subjectflags = gen->Flags;
-	Subjectflags |= CONTAINER_HAS_OBSERVER;
-	gen->Flags=Subjectflags;
-	memset(result,0,sizeof(Observer));
-	result->ObservedObject = ObservedObject;
-	result->Callback = callback;
-	result->Flags = flags;
-	if (ObserverVector == NULL && initVector() == 0) {
-		return CONTAINER_ERROR_NOMEMORY;
-	}
-	return 1;
-}
-
 static int Subscribe(void *ObservedObject, ObserverFunction callback, unsigned flags)
 {
-	Observer result;
-	int r;
-	r = InitObserver(&result,ObservedObject,callback,flags);
-	if (r > 0)
-		r = AddObject(&result);
-	return r;
+        size_t i;
+        Observer *pObs=NULL;
+        GenericContainer *gen = ObservedObject;
+        unsigned Subjectflags = gen->Flags;
+
+        Subjectflags |= CONTAINER_HAS_OBSERVER;
+        gen->Flags=Subjectflags;
+        if (ObserverVector == NULL) {
+		ObserverVector = calloc(sizeof(Observer),CHUNK_SIZE);
+		if (ObserverVector == NULL) {
+			iError.RaiseError("iObserver.Subscribe",CONTAINER_ERROR_NOMEMORY);
+	                return CONTAINER_ERROR_NOMEMORY;
+		}
+		vsize = CHUNK_SIZE;
+        }
+        for (i=0; i<vsize;i++) {
+                if (ObserverVector[i].ObservedObject==NULL) {
+			pObs = ObserverVector+i;
+			break;
+                }
+        }
+	if (i >= vsize) {
+        	Observer *tmp = realloc(ObserverVector,(vsize+CHUNK_SIZE)*sizeof(Observer));
+        	if (tmp == NULL) {
+               		iError.RaiseError("iObserver.Subscribe",CONTAINER_ERROR_NOMEMORY);
+                	return CONTAINER_ERROR_NOMEMORY;
+		}
+        	ObserverVector = tmp;
+        	memset(ObserverVector+vsize+1,0,(CHUNK_SIZE-1)*sizeof(Observer));
+		pObs = ObserverVector + vsize;
+        	vsize+= CHUNK_SIZE;
+	}
+	pObs->ObservedObject = ObservedObject;
+	pObs->Callback = callback;
+	pObs->Flags = flags;
+	return 1;
 }
 
 
 static int Notify(void *ObservedObject,unsigned operation,void *ExtraInfo1,void *ExtraInfo2)
 {
 	int count=0;
-	size_t idx = 0;
+	size_t idx;
 	void *ExtraInfo[2];
 
 	ExtraInfo[0] = ExtraInfo1;
@@ -93,31 +72,28 @@ static int Notify(void *ObservedObject,unsigned operation,void *ExtraInfo1,void 
 
 static size_t Unsubscribe(void *ObservedObject,ObserverFunction callback)
 {
-	size_t idx,count=0;
+	size_t idx=0,count=0;
 
 	if (ObservedObject == NULL) {
 		/* Erase all observers that have the specified function. This means that the
 		  object receiving the callback goes out of scope */
-		if (callback == NULL) /* If both are NULL do nothing */
-			return 0;
-		for (idx=0; idx<vsize;idx++) {
-			if (ObserverVector[idx].Callback == callback) {
-				memset(ObserverVector+idx,0,sizeof(Observer));
-				count++;
-			}
+		if (callback) /* If both are NULL do nothing */
+			for (; idx<vsize;idx++) {
+				if (ObserverVector[idx].Callback == callback) {
+					memset(ObserverVector+idx,0,sizeof(Observer));
+					count++;
+				}
 		}
-		return count;
 	}
-	if (callback == NULL) {
-		for (idx=0;idx<vsize;idx++) {
+	else if (callback == NULL) {
+		for (;idx<vsize;idx++) {
 			if (ObserverVector[idx].ObservedObject == ObservedObject) {
 				memset(ObserverVector+idx,0,sizeof(Observer));
 				count++;
 			}
 		}
-		return count;
 	}
-	for (idx=0; idx<vsize;idx++) {
+	else for (; idx<vsize;idx++) {
 		if (ObserverVector[idx].ObservedObject == ObservedObject &&
 			ObserverVector[idx].Callback == callback) {
 			memset(ObserverVector+idx,0,sizeof(Observer));
