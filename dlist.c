3b491abb0b24fce96dd9a4133baeadb3afa71fd1
Jacob Navia <jacob@jacob.remcomp.fr>
2020-05-03 12:06:24 +0200

    Cosmetic

--- dlist.c
+++ dlist.c
@@ -295,7 +295,7 @@ static int AddRange(Dlist * AL,size_t n, const void *data)
     if (AL->Flags & CONTAINER_HAS_OBSERVER)
         iObserver.Notify(AL,CCL_ADDRANGE,data,(void *)n);
 
-        return 1;
+	return 1;
 }
 
 /*------------------------------------------------------------------------
